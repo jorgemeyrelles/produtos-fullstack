cbe017f5fede4698900fcc7c56ac666c8f62152c
jorgemeyrelles <jotaengpuc@gmail.com>
2025-06-01 22:17:08 -0300

    feat: html consulta has been updated

--- webProdutos/src/app/app.config.ts
+++ webProdutos/src/app/app.config.ts
@@ -4,11 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
+import { produtosReducer } from './store/produtos/reducers/produtos.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { ProdutosEffects } from './store/produtos/effects/produtos.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideStore({ produtos: produtosReducer }),
+    provideEffects(ProdutosEffects),
   ],
 };
